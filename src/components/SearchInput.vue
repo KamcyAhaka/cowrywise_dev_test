66325877421bed3b6010fd1efa5f87ea764eefdf
Divine "KamCy" Ahaka <69519905+KamcyAhaka@users.noreply.github.com>
2025-02-20 16:52:00 +0100

    feat: implement site modifications
    > add return button for search input
    > add no photo data screen

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import MagnifyingGlass from './icons/MagnifyingGlass.vue'
+import ReturnIcon from './icons/ReturnIcon.vue';
 
 const emits = defineEmits(['search'])
 
@@ -22,26 +23,43 @@ const emitSearch = async (event: Event) => {
       type="search"
       name="image-search"
       id="image-search"
+      autofocus
       :placeholder="placeholder"
       @keyup.enter="emitSearch"
     />
+    <button @click="emitSearch">
+      <ReturnIcon />
+    </button>
   </div>
 </template>
 
 <style lang="scss" scoped>
+
+button {
+  background-color: transparent;
+  outline: none;
+  border: none;
+  cursor: pointer;
+}
+
 .search-input-container {
   height: auto;
   width: 100%;
   max-width: 1300px;
   margin: 0 auto;
-  background-color: #fefefe;
+  background-color: #f3f3f3;
   box-shadow: 0 5px 10px 0 rgba(0, 0, 0, 0.1);
   border-radius: 10px;
   display: flex;
   gap: 1rem;
   align-items: center;
   justify-content: center;
-  padding: 1.25rem 1.75rem;
+  padding: 1rem 1.75rem;
+  transition: background-color .3s ease;
+
+  &:has(input:focus) {
+    background-color: #ffffff;
+  }
 
   svg {
     color: #23345c;
@@ -54,10 +72,8 @@ const emitSearch = async (event: Event) => {
     font-weight: bold;
     font-size: clamp(14px, 14px + 0.1vw, 20px);
     color: #23345c;
-
-    &:focus {
-      background-color: #ffffff;
-    }
+    padding-block: .5em;
+    background-color: transparent;
 
     input::placeholder {
       color: #23345c;
